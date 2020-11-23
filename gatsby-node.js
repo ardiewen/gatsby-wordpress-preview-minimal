f28e64304c48da2632fed95b8985cdd9a27e8bb7
Ardie Wen <ardie.wen@hunterlily.co>
2020-11-23 19:49:54 +0000

    feat: create pages programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,35 @@
  */
 
 // You can delete this file if you're not using it
+
+const pageTemplate = require.resolve("./src/templates/page.js")
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const {
+    data: {
+      allWpPage: { nodes: pages },
+    },
+  } = await graphql(`
+    query GET_ALL_PAGES {
+      allWpPage(filter: { status: { eq: "publish" } }) {
+        nodes {
+          id
+          uri
+          title
+        }
+      }
+    }
+  `)
+
+  pages &&
+    pages.map(page => {
+      actions.createPage({
+        path: page.uri,
+        component: pageTemplate,
+        context: {
+          id: page.id,
+        },
+      })
+      reporter.info(`Page created: ${page.title} at ${page.uri}`)
+    })
+}

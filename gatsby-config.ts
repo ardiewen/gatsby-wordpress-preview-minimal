cd8f8a50fa8f522f19076e290d5f46cc397e855f
Ardie Wen <ardie.wen@hunterlily.co>
2021-01-26 23:32:12 +0000

    refactor: typescript support

---
+++ gatsby-config.ts
@@ -0,0 +1,42 @@
+export const siteMetadata = {
+  title: `Gatsby Default Starter`,
+  description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+  author: `@gatsbyjs`,
+}
+
+export const plugins = [
+  // `gatsby-plugin-typegen`,
+  `gatsby-plugin-react-helmet`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
+    },
+  },
+  {
+    resolve: "gatsby-source-wordpress-experimental",
+    options: {
+      url: process.env.GATSBY_GRAPHQL_URL,
+      verbose: true,
+      schema: {
+        requestConcurrency: 5,
+        previewRequestConcurrency: 2,
+      },
+    },
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `gatsby-starter-default`,
+      short_name: `starter`,
+      start_url: `/`,
+      background_color: `#663399`,
+      theme_color: `#663399`,
+      display: `minimal-ui`,
+      icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    },
+  },
+]
---
+++ gatsby-node.ts
@@ -0,0 +1,13 @@
+import type { GatsbyNode } from "gatsby"
+import { createWpPages } from "./src/create/createWpPages"
+import { createWpPosts } from "./src/create/createWpPosts"
+// import { createWpCaseStudies } from "./src/create/createWpCaseStudies"
+
+export const createPages: GatsbyNode["createPages"] = async methods => {
+  // Create Pages
+  await createWpPages(methods)
+  // Create Posts
+  await createWpPosts(methods)
+  // Create CPTs (Case Studies)
+  // await createWpCaseStudies(methods)
+}
